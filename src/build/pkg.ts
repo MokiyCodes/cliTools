73dd5601e0aa6e8c8776133523d14dfadb077273
MokiyCodes <mokiycodes@users.noreply.github.com>
2022-12-12 12:56:18 +0100

    fix: await da exec

--- src/build/pkg.ts
+++ src/build/pkg.ts
@@ -2,7 +2,9 @@ import { readdirSync } from "fs";
 import { resolve } from "path";
 import * as pkg from 'pkg'
 const files = readdirSync(resolve(__dirname,'..')).filter(v=>v.endsWith('.js')).map(v=>resolve(__dirname,'..',v));
-for (const file of files) {
-  console.log('Compiling',file);
-  pkg.exec([file,'-t','node18','--output',`${resolve(process.cwd(),'dist',`${file.split('\\').pop().split('/').pop().split('.').shift()}`)}${process.platform==='win32'?'.exe':''}`])
-}
+(async()=>{
+  for (const file of files) {
+    console.log('Compiling',file);
+    await pkg.exec([file,'-t','node18','--output',`${resolve(process.cwd(),'dist',`${file.split('\\').pop().split('/').pop().split('.').shift()}`)}${process.platform==='win32'?'.exe':''}`])
+  }
+})()
