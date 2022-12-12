f422499b0561425824fefdc65a3399f8a6ca9856
MokiyCodes <mokiycodes@users.noreply.github.com>
2022-12-12 12:18:35 +0100

    fix: We only check for  bins, man pages are ignored, so non-win32 was fundamentally flawed

--- src/tests/where-is.ts
+++ src/tests/where-is.ts
@@ -3,7 +3,4 @@ import { existsSync } from "fs";
 import { resolve } from "path";
 
 const out = execSync(`node ${resolve(__dirname,'..','where-is')} node`)
-if (process.platform !== 'win32'){
-  if (out.toString('utf-8').trim() !== execSync('whereis node').toString('utf-8').trim())
-    throw new Error('does not match builtin whereis output')
-} else if (out.toString('utf-8').split(' ').filter((_,idx)=>idx>0).filter(v=>!existsSync(v)).length>0) throw new Error('cannot find one of the reported files')
+if (out.toString('utf-8').split(' ').filter((_,idx)=>idx>0).filter(v=>!existsSync(v)).length>0) throw new Error('cannot find one of the reported files')
