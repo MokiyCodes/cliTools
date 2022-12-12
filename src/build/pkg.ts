e0f21a78a8a84b50b184fc8f412aa1b44e5b2f2f
MokiyCodes <mokiycodes@users.noreply.github.com>
2022-12-12 12:42:25 +0100

    feat: Add builds to CI

---
+++ src/build/pkg.ts
@@ -0,0 +1,8 @@
+import { readdirSync } from "fs";
+import { resolve } from "path";
+import * as pkg from 'pkg'
+const files = readdirSync(resolve(__dirname,'..')).filter(v=>v.endsWith('.js')).map(v=>resolve(__dirname,'..',v));
+for (const file of files) {
+  console.log('Compiling',file);
+  pkg.exec([file,'-t','node18','--output',`${resolve(process.cwd(),'dist',`${file.split('\\').pop().split('/').pop().split('.').shift()}`)}${process.platform==='win32'?'.exe':''}`])
+}
